5130908c3d294df667fdd5c2a30e79d69f4e089c
erambeau <rambeaue@gmail.com>
2022-07-12 15:36:45 +0200

    Create & Show event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,37 @@
 class EventsController < ApplicationController
 
+    before_action :authenticate_user!, except: [:index, :show]
+
     def index
         @events = Event.all
     end
+
+    def show
+        @event = Event.find(params[:id])
+        @users = User.all
+    end
+
+
+    def new
+        @event = Event.new
+    end
+
+
+    def create
+        @event = current_user.events.build(event_params)
+
+        if @event.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+        
+    end
+
+
+    private
+    def event_params
+      params.require(:event).permit(:date_event, :location, :creator_id)
+    end
+
 end
